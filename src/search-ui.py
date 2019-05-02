72fb62515f820faf68772ae869dae3a5506676c0
Sean-Hassett <hassetse@tcd.ie>
2019-05-02 22:54:54 +0100

    printing which boxes are checked

--- src/search-ui.py
+++ src/search-ui.py
@@ -7,7 +7,7 @@ CHECK_BOX_HEIGHT = 20
 BUTTON_HEIGHT = 30
 OFFSET = 25
 CHECK_BOX_OFFSET = 15
-CHECK_BOXES = ["{}Chords", "{}Tab", "{}GuitarPro", "{}Power", "{}Bass", "{}Ukulele"]
+CHECK_BOX_NAMES = ["{}Chords", "{}Tab", "{}GuitarPro", "{}Power", "{}Bass", "{}Ukulele"]
 
 
 # noinspection PyUnresolvedReferences
@@ -30,15 +30,16 @@ class UiSearchWindow(object):
         self.search_input.setObjectName("lineEdit")
 
         self.check_boxes = [" "] * 6
-        for i, check_box in enumerate(CHECK_BOXES):
+        for i, name in enumerate(CHECK_BOX_NAMES):
             self.check_boxes[i] = QtWidgets.QCheckBox(self.central_widget)
             self.check_boxes[i].setGeometry(QtCore.QRect(OFFSET, ((OFFSET * 2 + TEXT_BOX_HEIGHT) + (CHECK_BOX_HEIGHT + CHECK_BOX_OFFSET) * i), WIDTH, CHECK_BOX_HEIGHT))
-            self.check_boxes[i].setObjectName(check_box.format("checkBox"))
-            self.check_boxes[i].setText(QtCore.QCoreApplication.translate("UiSearchWindow", check_box.format("")))
+            self.check_boxes[i].setObjectName(name.format("checkBox"))
+            self.check_boxes[i].setText(QtCore.QCoreApplication.translate("UiSearchWindow", name.format("")))
 
         self.push_button = QtWidgets.QPushButton(self.central_widget)
         self.push_button.setGeometry(QtCore.QRect(25, 300, 150, 30))
-        self.push_button.setObjectName("pushButton")
+        self.push_button.setObjectName("searchButton")
+        self.push_button.clicked.connect(self.print_check_boxes)
 
         search_window.setCentralWidget(self.central_widget)
 
@@ -50,6 +51,11 @@ class UiSearchWindow(object):
         window.setWindowTitle(_translate("UiSearchWindow", "Tab Search"))
         self.search_input.setPlaceholderText(_translate("UiSearchWindow", "Search query..."))
         self.push_button.setText(_translate("UiSearchWindow", "Search"))
+
+    def print_check_boxes(self):
+        for check_box in self.check_boxes:
+            if check_box.isChecked():
+                print(check_box.text())
 
 
 if __name__ == "__main__":
