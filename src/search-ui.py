ed378656c353af1315a06fe26ff479727bd11a76
Sean-Hassett <hassetse@tcd.ie>
2019-05-02 22:16:53 +0100

    removed second text box from search ui

--- src/search-ui.py
+++ src/search-ui.py
@@ -4,8 +4,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class UiSearchWindow(object):
     def setupUi(self, SearchWindow):
         SearchWindow.setObjectName("SearchWindow")
-        SearchWindow.setMinimumSize(QtCore.QSize(200, 400))
-        SearchWindow.setMaximumSize(QtCore.QSize(200, 400))
+        SearchWindow.setMinimumSize(QtCore.QSize(200, 355))
+        SearchWindow.setMaximumSize(QtCore.QSize(200, 355))
         font = QtGui.QFont()
         font.setPointSize(12)
         SearchWindow.setFont(font)
@@ -19,43 +19,38 @@ class UiSearchWindow(object):
         self.lineEdit.setFont(font)
         self.lineEdit.setObjectName("lineEdit")
 
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(25, 70, 150, 30))
-        self.lineEdit_2.setFont(font)
-        self.lineEdit_2.setObjectName("lineEdit_2")
-
         self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox.setGeometry(QtCore.QRect(25, 125, 150, 20))
+        self.checkBox.setGeometry(QtCore.QRect(25, 80, 150, 20))
         self.checkBox.setFont(font)
         self.checkBox.setObjectName("checkBox")
 
         self.checkBox_2 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_2.setGeometry(QtCore.QRect(25, 160, 150, 20))
+        self.checkBox_2.setGeometry(QtCore.QRect(25, 115, 150, 20))
         self.checkBox_2.setFont(font)
         self.checkBox_2.setObjectName("checkBox_2")
 
         self.checkBox_3 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_3.setGeometry(QtCore.QRect(25, 195, 150, 20))
+        self.checkBox_3.setGeometry(QtCore.QRect(25, 150, 150, 20))
         self.checkBox_3.setFont(font)
         self.checkBox_3.setObjectName("checkBox_3")
 
         self.checkBox_4 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_4.setGeometry(QtCore.QRect(25, 230, 150, 20))
+        self.checkBox_4.setGeometry(QtCore.QRect(25, 185, 150, 20))
         self.checkBox_4.setFont(font)
         self.checkBox_4.setObjectName("checkBox_4")
 
         self.checkBox_5 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_5.setGeometry(QtCore.QRect(25, 265, 150, 20))
+        self.checkBox_5.setGeometry(QtCore.QRect(25, 220, 150, 20))
         self.checkBox_5.setFont(font)
         self.checkBox_5.setObjectName("checkBox_5")
 
         self.checkBox_6 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_6.setGeometry(QtCore.QRect(25, 300, 150, 20))
+        self.checkBox_6.setGeometry(QtCore.QRect(25, 255, 150, 20))
         self.checkBox_6.setFont(font)
         self.checkBox_6.setObjectName("checkBox_6")
 
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(25, 345, 150, 30))
+        self.pushButton.setGeometry(QtCore.QRect(25, 300, 150, 30))
         self.pushButton.setFont(font)
         self.pushButton.setObjectName("pushButton")
 
@@ -67,8 +62,7 @@ class UiSearchWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("SearchWindow", "SearchWindow"))
-        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Artist"))
-        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "Song Title"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Search query..."))
         self.checkBox.setText(_translate("MainWindow", "Chords"))
         self.checkBox_2.setText(_translate("MainWindow", "Tab"))
         self.checkBox_3.setText(_translate("MainWindow", "GuitarPro"))
