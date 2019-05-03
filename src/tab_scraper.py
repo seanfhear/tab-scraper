abd96bd535dc1de72c82f9e43e993f44c97839b4
Sean-Hassett <hassetse@tcd.ie>
2019-05-03 17:30:06 +0100

    added pagination to scraper

--- src/tab_scraper.py
+++ src/tab_scraper.py
@@ -1,40 +1,50 @@
 import requests
 import re
 import json
-import sys
 # noinspection PyUnresolvedReferences
 from PyQt5 import QtGui
 
-# TODO pagination
 # TODO remove warning suppressions
 
-SEARCH_URL = "https://www.ultimate-guitar.com/search.php?search_type=title&value={}"
+SEARCH_URL = "https://www.ultimate-guitar.com/search.php?page={}&search_type=title&value={}"
 RESULTS_PATTERN = "\"results\":(\[.*?\]),\"pagination\""
+RESULTS_COUNT_PATTERN = "\"tabs\",\"results_count\":([0-9]+?),\"results\""
 
 
 def search_tabs(search_string, types):
-    response = requests.get(SEARCH_URL.format(search_string))
+    page = 1
+    # get first page of results
+    response = requests.get(SEARCH_URL.format(page, search_string))
+    count = 0
     try:
         # isolate results from page using regex
-        results = re.search(RESULTS_PATTERN, response.content.decode()).group(1)
+        response_body = response.content.decode()
+        results = re.search(RESULTS_PATTERN, response_body).group(1)
+        count = int(re.search(RESULTS_COUNT_PATTERN, response_body).group(1))
     except AttributeError:
         results = ''
     response_data = json.loads(results)
     ret = []
 
-    for item in response_data:
-        try:
-            # Get every result that has a desired type defined above
-            if item["type"] in types:
-                ret.append((item["type"], item["artist_name"], item["song_name"], str(round(float(item["rating"]), 2)),
-                            str(item["votes"])))
-                print("Type: {}".format(item["type"]))
-                print("Artist: {}".format(item["artist_name"]))
-                print("Name: {}".format(item["song_name"]))
-                print("{} Rating from {} Votes".format(str(round(float(item["rating"]), 2)), item["votes"]))
-                print(item["tab_url"])
-                print()
-        except KeyError:
-            # key error on "official" tabs which have 'marketing_type' instead of 'type', not interested in these tabs
-            ''
+    while count > 0:
+        for item in response_data:
+            try:
+                # Get every result that has a desired type
+                if item["type"] in types:
+                    ret.append((item["type"], item["artist_name"], item["song_name"], str(round(float(item["rating"]), 2)),
+                                str(item["votes"])))
+            except KeyError:
+                # key error on "official" tabs which have 'marketing_type' instead of 'type', not interested in these tabs
+                ''
+            count -= 1
+        if count > 0:
+            page += 1
+            response = requests.get(SEARCH_URL.format(page, search_string))
+            try:
+                # isolate results from page using regex
+                response_body = response.content.decode()
+                results = re.search(RESULTS_PATTERN, response_body).group(1)
+            except AttributeError:
+                results = ''
+            response_data = json.loads(results)
     return ret
